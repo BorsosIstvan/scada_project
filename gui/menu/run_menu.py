c92e57eb45478d6d81bb7c2b44e2f6847e45c317
BorsosIstvan <bistvanus@gmail.com>
2025-04-11 22:33:50 +0200

    opruimen netjes

--- gui/menu/run_menu.py
+++ gui/menu/run_menu.py
@@ -2,19 +2,18 @@ from tkinter import Menu
 from gui.menu.dialogs.communicatie_dialog import open_communicatie_settings as communicatie_dialoog
 
 
-
 def create_run_menu(menubar, object_manager):
     run_menu = Menu(menubar, tearoff=0)
 
     def start_simulatie():
         print("Simulatie gestart")
-        if hasattr(simulatie_manager, "start_simulatie"):
+        if hasattr(object_manager, "start_simulatie"):
             object_manager.start_simulatie()
 
     def stop_simulatie():
         print("Simulatie gestopt")
-        if hasattr(simulatie_manager, "stop_simulatie"):
-            simulatie_manager.stop_simulatie()
+        if hasattr(object_manager, "stop_simulatie"):
+            object_manager.stop_simulatie()
 
     def open_communicatie_settings():
         communicatie_dialoog(object_manager)
