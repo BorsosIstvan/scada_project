9e3fb8d364eb65b479986982db841967ee08d416
BorsosIstvan <bistvanus@gmail.com>
2025-04-11 23:33:25 +0200

    allen maar zo werkt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # SCADA app main.py
 import tkinter as tk
 from managers.object_manager import ObjectManager
-from managers.simulatie_manager import SimulatieManager
 from gui.menu.menu_manager import MenuManager
 from config import (
     WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE_BASE, WINDOW_BG_COLOR
@@ -10,6 +9,8 @@ from config import (
 
 class SCADAApp:
     def __init__(self, root):
+        self.update_title = None
+        self.modbus = None
         self.root = root
         self.root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
         self.root.title(f"{WINDOW_TITLE_BASE} - Nieuw")
@@ -38,7 +39,6 @@ class SCADAApp:
             self.root.title(f"{WINDOW_TITLE_BASE} - {self.current_name}{status}")
 
         self.object_manager = ObjectManager(self.canvas, update_title)
-        self.simulatie_manager = SimulatieManager(self.canvas, update_title)
         self.menu = MenuManager.create_menu(root, self.canvas, self.object_manager, set_name)
 
 
